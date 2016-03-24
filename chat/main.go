76c984e6fbe6ceee3bd2c81cf097061a57631cfb
Hara Atsushi <toohsk@gmail.com>
2016-03-24 19:32:32 +0900

    add httpHandler using template engine

---
+++ chat/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+// テンプレートの読み込み。一度だけ行われる
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTPはHTTPリクエストを処理する
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
