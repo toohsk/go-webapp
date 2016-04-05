38bbedee15197de2c316bbe6d98152298deb512b
toohsk <toohsk@gmail.com>
2016-04-06 00:44:54 +0900

    add chat client

--- chat/main.go
+++ chat/main.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"sync"
 	"text/template"
+	"flag"
 )
 
 // テンプレートの読み込み。一度だけ行われる
@@ -20,10 +21,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+    var addr = flag.String("addr", ":8080", "address of application")
+    flag.Parse()
+    
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
@@ -31,7 +35,8 @@ func main() {
 	go r.run()
 
 	// start web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+    log.Println("Webサーバを開始します。ポート: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
